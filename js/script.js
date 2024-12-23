cd4d09a9fe6253cf5ed7ba2a67f3366de90ed44e
ArefeEtesami <arefeetesamii@gmail.com>
2024-12-23 22:44:19 +0330

    intro

--- js/script.js
+++ js/script.js
@@ -14,18 +14,46 @@ document.addEventListener("click", function (event) {
 });
 
 // show profile popup
-document.getElementById("profile-img").addEventListener("click", function(event) {
+document.getElementById("profile-img").addEventListener("click", function (event) {
     var profilePopup = document.getElementById("profile-popup");
     profilePopup.classList.toggle("show");
     event.stopPropagation();
 });
 
 // close profile popup
-document.addEventListener("click", function(event) {
+document.addEventListener("click", function (event) {
     var profilePopup = document.getElementById("profile-popup");
     var profileImg = document.getElementById("profile-img");
-  
+
     if (!profilePopup.contains(event.target) && !profileImg.contains(event.target)) {
-      profilePopup.classList.remove("show");
+        profilePopup.classList.remove("show");
     }
-  });
+});
+
+// hide and show the search label 
+const searchInput = document.querySelector('.search-input');
+const searchLabel = document.querySelector('.search-label');
+searchInput.addEventListener('input', function() {
+    if (searchInput.value.trim() !== '') {
+        searchLabel.style.display = 'none';
+    } else {
+        searchLabel.style.display = 'flex'; 
+    }
+});
+
+// select and deselect category spans
+document.addEventListener('DOMContentLoaded', () => {
+    const spans = document.querySelectorAll('.category-span');
+
+    spans.forEach(span => {
+        span.addEventListener('click', () => {
+            spans.forEach(s => {
+                s.classList.remove('selected-category-span');
+                s.classList.add('deselected-category-span');
+            });
+
+            span.classList.remove('deselected-category-span');
+            span.classList.add('selected-category-span');
+        });
+    });
+});
