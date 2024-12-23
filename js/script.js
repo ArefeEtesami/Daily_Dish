c54b5de2852dd22397ee64426de3278d10143806
ArefeEtesami <arefeetesamii@gmail.com>
2024-12-23 21:27:48 +0330

    header 2

--- js/script.js
+++ js/script.js
@@ -12,3 +12,20 @@ document.addEventListener("click", function (event) {
         mobileHeader.classList.remove("open");
     }
 });
+
+// show profile popup
+document.getElementById("profile-img").addEventListener("click", function(event) {
+    var profilePopup = document.getElementById("profile-popup");
+    profilePopup.classList.toggle("show");
+    event.stopPropagation();
+});
+
+// close profile popup
+document.addEventListener("click", function(event) {
+    var profilePopup = document.getElementById("profile-popup");
+    var profileImg = document.getElementById("profile-img");
+  
+    if (!profilePopup.contains(event.target) && !profileImg.contains(event.target)) {
+      profilePopup.classList.remove("show");
+    }
+  });
